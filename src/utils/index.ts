428284acce2c62be5d39f94a27dc59688c385b44
Nagy Viktor <nagy.viktor@fotex.net>
2023-08-30 09:12:43 +0200

    fix: added missing exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,8 @@
 export { default as capitalize } from './capitalize/capitalize';
+export { default as currency } from './currency/currency';
 export { default as dayjs2string } from './dayjs2string/dayjs2string';
+export { default as inclusiveRandom } from './inclusiveRandom/inclusiveRandom';
+export { default as findLastIndex } from './findLastIndex/findLastIndex';
 export { default as mergeMap } from './mergeMap/mergeMap';
+export { default as random } from './random/random';
 export { default as omit } from './omit/omit';
